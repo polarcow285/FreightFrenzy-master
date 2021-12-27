385febfba7dbe103aefb2a2f23693568956245a0
Natalie Leung <mmoopal@gmail.com>
2021-12-26 20:42:04 -0800

    Used OpenCV simulator to make shipping element detecting code. Initialized camera (untested) in intermediate auto opmode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/IntermediateAuto.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Vision.ShippingElementDetector;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import org.firstinspires.ftc.teamcode.Projects.ProjectOdometryTest;
+
+@Autonomous(name = "IntermediateAuto")
+public class IntermediateAuto extends LinearOpMode {
+    public ProjectOdometryTest robot = new ProjectOdometryTest();
+    OpenCvWebcam webcam;
+    ShippingElementDetector detector = new ShippingElementDetector(telemetry);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
+
+        webcam.setPipeline(detector);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                // Usually this is where you'll want to start streaming from the camera (see section 4)
+                webcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        Path p = Path.Red;
+
+        while(!isStarted()) {
+            if (gamepad1.b) {
+                p = Path.Red;
+            }
+            if (gamepad1.x) {
+                p = Path.Blue;
+            }
+            telemetry.addData("Path: ", p);
+            telemetry.update();
+        }
+
+        waitForStart();
+        /*if(detector.getShippingElementLocation() == ShippingElementDetector.ShippingElementLocation.LEFT){
+
+        }*/
+
+        switch (detector.getShippingElementLocation()) {
+            case LEFT: case UNKNOWN:
+                // ...
+                break;
+            case MIDDLE:
+                // ...
+                break;
+            case RIGHT:
+                // ...
+        }
+        webcam.stopStreaming();
+
+
+
+    }
+}
