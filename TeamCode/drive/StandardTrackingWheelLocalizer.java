52ae0f4999849d4d2fc3eeb73c42716a72e9c15b
Natalie Leung <mmoopal@gmail.com>
2022-01-20 16:15:29 -0800

    RoadRunner config data inputted. Ready for tuning.

--- TeamCode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/drive/StandardTrackingWheelLocalizer.java
@@ -27,12 +27,12 @@ import java.util.List;
  */
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 0;
-    public static double WHEEL_RADIUS = 2; // in
+    public static double TICKS_PER_REV = 8192;
+    public static double WHEEL_RADIUS = 0.98; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 2.27; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 5.173; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
