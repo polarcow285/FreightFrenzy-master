4fafb914b0071ccfacc499902013d8ee00fb1389
Natalie Leung <mmoopal@gmail.com>
2022-02-17 20:14:36 -0800

    Tuned PID coefficients

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,8 +34,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-      getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(38, 0, 13,
+      11.5);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 9.85560812330105;
+    public static double MAX_VEL = 141.3;
     public static double MAX_ACCEL = 9.85560812330105;
     public static double MAX_ANG_VEL = Math.toRadians(64.5354);
     public static double MAX_ANG_ACCEL = Math.toRadians(64.5354);
